e304e2850cd95701c6a551f74f1d43d4ba272857
Kamal Tej <31327193+Kamaltejveerapaneni@users.noreply.github.com>
2018-09-07 17:40:06 -0500

    ICP 3
    commit

--- In Class Exercises/ICP 3/source/1-b.py
+++ In Class Exercises/ICP 3/source/1-b.py
@@ -5,9 +5,9 @@ from collections import Counter
 codict = {}
 with open("C:\\Users\kamal\Documents\Github\Python-and-Deep-Learning\In Class Exercises\ICP 3\source\codon.tsv") as tsvfile:#read from tsv file
   reader = csv.reader(tsvfile, delimiter='\t')
-  for cols in reader:
+  for cols in reader:#create dictionary
     codict[cols[0]] = cols[1]
-  sequence = input(" enter the input sequence: ")
+  sequence = input(" enter the input sequence: ")#input sequence
 
   codons = (re.findall(r'.{1,3}', sequence, re.DOTALL))
   print("the individual codon sequence is: ", codons)
